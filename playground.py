1925fd023a45a23fb3ed25ff401356ad31e1424b
vishesh147 <vishesh.r.patel@gmail.com>
2024-04-30 18:02:18 +0530

    Cleanup

--- playground.py
+++
@@ -1,8 +0,0 @@
-from app.text_matcher import TextAddress
-from fuzzywuzzy import fuzz
-import fuzzy
-
-addr1 = TextAddress("Narmada Bhuvan")
-addr2 = TextAddress("Naramada Bhavan")
-
-print(addr1.compare_fuzz(addr2))
